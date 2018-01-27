e166f5fc6f6d64f0d8a9c928a2d105516f780221
Erick-Pines <erickterana@gmail.com>
2018-01-26 18:13:40 -0600

    Prueba

---
+++ Arduino_Joystick/Arduino_Joystick.ino
@@ -0,0 +1,41 @@
+//Prueba para múltiples switches. 
+
+#include <Joystick.h>
+
+Joystick_ Joystick;
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 9;
+
+// Last state of the button
+int lastButtonState[4] = {0,0,0,0};
+
+void loop() {
+  for (int index = 0; index < 9; index++)
+  {
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
+
