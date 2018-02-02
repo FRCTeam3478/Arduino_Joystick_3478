c45d0e7cc76c975872c36b958c398182089475ce
juanct <jctfjctf@gmail.com>
2018-02-02 16:31:57 -0600

    Se termino el codigo y ya se mapeo para la placa real

--- Arduino_Joystick/Arduino_Joystick.ino
+++ Arduino_Joystick/Arduino_Joystick.ino
@@ -2,40 +2,42 @@
 
 #include <Joystick.h>
 
+////crea el objeto de joystick
 Joystick_ Joystick;
-
-void setup() {
-  // Initialize Button Pins
-  pinMode(9, INPUT_PULLUP);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-
-  // Initialize Joystick Library
-  Joystick.begin();
-}
 
 // Constant that maps the phyical pin to the joystick button.
 const int pinToButtonMap = 9;
+int index = 0;
 
-// Last state of the button
-int lastButtonState[4] = {0,0,0,0};
+int lastButtonState[12] = {0,0,0,0,0,0,0,0,0,0,0,0};  //ultimos estados
+int Buttons_map[12] = {0,1,2,3,4,5,6,7,8,9,10,11}; //botones a ller del joystick (0 aqui es el boton 1, 1 el 2, etc)
+int Pins_map[12] = {9,8,7,6,5,4,3,2,A3,A2,A1,A0};  //pines del arduino
 
+////////////codigo de setup/////////////
+void setup() {
+  // Initialize Button Pins
+  for( index = 0; index < 12; index++){
+    pinMode(Pins_map[index], INPUT_PULLUP);
+  }
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+///////////////////////////////////////
+
+
+/////////codigo principal///////////////////
 void loop() {
-  for (int index = 0; index < 9; index++)
+  for ( index = 0; index < 12; index++)  //checa todos los botones
   {
-    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    int currentButtonState = !digitalRead(Pins_map[index]);
     if (currentButtonState != lastButtonState[index])
     {
-      Joystick.setButton(index, currentButtonState);
+      Joystick.setButton(Buttons_map[index], currentButtonState);
       lastButtonState[index] = currentButtonState;
     }
   }
 
-  delay(50);
+  delay(10);
 }
+////////////////////////////////////////
 
